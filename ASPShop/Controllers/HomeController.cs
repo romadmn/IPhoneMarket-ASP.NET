dae5ed5d78532b454dca2091e3d67be3d9a7913f
romadmn <ferencrman@gmail.com>
2020-01-30 16:53:25 +0200

    Add Phone Data and Views for buying

--- ASPShop/Controllers/HomeController.cs
+++ ASPShop/Controllers/HomeController.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPShop.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASPShop.Controllers
 {
     public class HomeController : Controller
     {
+        MobileContext db;
+        public HomeController(MobileContext context)
+        {
+            db = context;
+        }
         public IActionResult Index()
         {
+            return View(db.Phones.ToList());
+        }
+        [HttpGet]
+        public IActionResult Buy(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.PhoneId = id;
             return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return "Дякую, " + order.User + ", за покупку!";
         }
     }
 }
